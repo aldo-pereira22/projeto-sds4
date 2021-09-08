59423235b9308c9600a73bcdf42c7e81c69a0059
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-09-08 10:03:13 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://www.instagram.com/aldopereira.dev22/" target="_blank" rel="noreferrer">Aldo Pereira</a>  </p>
+            <p className="text-light">GitHub <a href="https://github.com/aldo-pereira22" target="_blank" rel="noreferrer">Aldo Pereira</a></p>
+
+        </div>
+        </footer>
+
+    );
+  }
+  
+export default Footer;
+  
